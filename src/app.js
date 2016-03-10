106844dd9df60a7d72d3fd4321273c95b46e8ed1
Andrew Stoker <TheAndyBob@me.com>
2016-03-10 11:28:33 -0500

    demonstrating dynamic routes not updating getters

--- src/app.js
+++ src/app.js
@@ -4,33 +4,42 @@ export class App {
     configureRouter(config, router) {
 
 
+        //Comment out these lines below (until promise) to see the routes actually show up
+        this.router = router;
+        config.title = 'Aurelia';
+        config.map([
+          { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' , settings: {group: 'Welcome'}},
+          { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' , settings: {group: 'Users'}},
+          //{ route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' , settings: {group: 'Child'}}
+        ]);
 
-
-      //Mimicking an Api call to get user permissions for additional routes
-      return new Promise((resolve, reject)=>{
+      //Mimicking an Api call to get user permissions for additional routes.
+      //Must return this promise in order to show data correctly
+      //return new Promise((resolve, reject)=>{
+      new Promise((resolve, reject)=>{
           console.log('setting config');
 
 
+
           setTimeout(() => {
-              this.router = router;
               window.router = this.router;
-
-
-                config.title = 'Aurelia';
-                config.map([
-                  { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' , settings: {group: 'Welcome'}},
-                  { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' , settings: {group: 'Users'}},
-                   { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router', settings: {group: 'Child'} }
-                ]);
+                //Uncomment these lines to get routing to work correctly
+                // this.router = router;
+                // config.title = 'Aurelia';
+                // config.map([
+                //   { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' , settings: {group: 'Welcome'}},
+                //   { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' , settings: {group: 'Users'}},
+                //   { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' , settings: {group: 'Child'}}
+                // ]);
 
               console.log('finished config');
-              //this.router.addRoute({ route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router', settings: {group: 'Child'} });
-              //this.router.refreshNavigation();
+              this.router.addRoute({ route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router', settings: {group: 'Child'} });
+              this.router.refreshNavigation();
 
               //this.createNavigationGroups();
 
               resolve(true);
-          }, 4000)
+          }, 2000)
 
       });
 
