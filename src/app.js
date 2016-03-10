375ee52d467a2a6cb7db679c247a5d94d9c7e997
Andrew Stoker <TheAndyBob@me.com>
2016-03-10 11:12:48 -0500

    Updates

--- src/app.js
+++ src/app.js
@@ -3,22 +3,29 @@ export class App {
 
     configureRouter(config, router) {
 
-      this.router = router;
-      config.title = 'Aurelia';
-      config.map([
-        { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' , settings: {group: 'Welcome'}},
-        { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' , settings: {group: 'Users'}},
-        // { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
-      ]);
 
-      window.router = this.router;
+
+
       //Mimicking an Api call to get user permissions for additional routes
-      new Promise((resolve, reject)=>{
+      return new Promise((resolve, reject)=>{
           console.log('setting config');
+
+
           setTimeout(() => {
-             console.log('finished config');
-              this.router.addRoute({ route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router', settings: {group: 'Child'} });
-              this.router.refreshNavigation();
+              this.router = router;
+              window.router = this.router;
+
+
+                config.title = 'Aurelia';
+                config.map([
+                  { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' , settings: {group: 'Welcome'}},
+                  { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' , settings: {group: 'Users'}},
+                   { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router', settings: {group: 'Child'} }
+                ]);
+
+              console.log('finished config');
+              //this.router.addRoute({ route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router', settings: {group: 'Child'} });
+              //this.router.refreshNavigation();
 
               //this.createNavigationGroups();
 
